e59ee80e5507a5798d8adcc507b50032f0e25f8d
Raimones Barros <raimonesrg3@gmail.com>
2023-09-26 12:20:28 -0300

    refactor: improves pwa

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -58,7 +58,23 @@ export default class MyDocument extends Document {
               content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
             />
             <meta name="theme-color" content="#45C7AF" />
-            <link rel="icon" href="favicon.svg" type="image/x-icon" />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
             <link rel="manifest" href="/manifest.json" />
           </Head>
           <body>
