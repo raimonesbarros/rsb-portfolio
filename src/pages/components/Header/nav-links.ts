2fbc0a094eb4ebface0363874314ca669f5f6a49
Raimones Barros <raimonesrg3@gmail.com>
2023-10-04 21:32:23 -0300

    refactor: header change links

---
+++ src/pages/components/Header/nav-links.ts
@@ -0,0 +1,32 @@
+export const links = [
+  {
+    href: "/#",
+    to: "showcase",
+    content: "INÍCIO",
+    label: "ir para o inicio",
+  },
+  {
+    href: "/projects",
+    to: "/",
+    content: "PROJETOS",
+    label: "ir para projetos",
+  },
+  {
+    href: "/blog",
+    to: "/",
+    content: "BLOG",
+    label: "ir para blog",
+  },
+  {
+    href: "/#about",
+    to: "about",
+    content: "SOBRE",
+    label: "ir para sessão sobre",
+  },
+  {
+    href: "/#contact",
+    to: "contact",
+    content: "CONTATO",
+    label: "ir para sessão contatos",
+  },
+];
